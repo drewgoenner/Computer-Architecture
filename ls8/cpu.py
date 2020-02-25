9d85cad3d1068a42917086c143d8c1baad7c8948
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-02-25 14:41:02 -0600

    Completed Day 2 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,12 @@
 
 import sys
 
+#create instructions for LDI, PRN, HLT, and MUL programs
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 
 class CPU:
     """Main CPU class."""
@@ -12,27 +18,51 @@ class CPU:
         self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
+        self.branchtable = {}
+        self.branchtable[HLT] = self.handle_hlt
+        self.branchtable[LDI] = self.handle_ldi
+        self.branchtable[PRN] = self.handle_prn
+        self.branchtable[MUL] = self.handle_mul
+        self.halted = False
 
     def load(self):
         """Load a program into memory."""
 
+        if len(sys.argv) != 2:
+            print("format: ls8.py [filename]")
+            sys.exit(1)
+
+        program = sys.argv[1]
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        #open file
+        with open(program) as file:
+            #read the lines
+            for line in file:
+                #parse out comments
+                line = line.strip().split("#")
+                #cast numbers from strings to ints
+                val = line[0].strip()
+                #ignore blank lines
+                if line == "":
+                    continue
+
+                value = int(val, 2)
+                self.ram[address] = value
+                address +=1
+        
 
 
     def alu(self, op, reg_a, reg_b):
@@ -40,7 +70,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.register[reg_a] *= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -66,44 +97,9 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        #create instructions for LDI, PRN, and HLT programs
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
+        
         #set running to True
         running = True
-
-        #while cpu is running
-        while running:
-            #  set instruction register per step 3
-            IR = self.ram[self.pc]
-
-            # set operand_a to pc+1 per step 3
-            operand_a = self.ram_read(self.pc + 1)
-            # set operand_b to pc+2 per step 3
-            operand_b = self.ram_read(self.pc + 2)
-
-            # if the instruction register is LDI
-            if IR == LDI:
-                #set register of operand_a to operand_b, jump 3 in PC (to PRN currently)
-                self.register[operand_a] = operand_b
-                self.pc +=3
-
-            # if the instruction register is PRN
-            elif IR == PRN:
-                #print the register of operand_a, jump 2 in PC
-                print(self.register[operand_a])
-                self.pc +=2
-
-            # if the instruction register is the halt command
-            elif IR == HLT:
-                #set running to false and exit
-                running = False
-                sys.exit(0)
-            # if anything else, invalid command and quit with failure code 1
-            else:
-                print(f"Invalid Command: {self.ram[self.pc]}")
-                sys.exit(1)
 
     #ram_read should accept an address to read and return the value
     def ram_read(self, mar):
@@ -113,3 +109,70 @@ class CPU:
     def ram_write(self, mdr, mar):
         self.ram[mar] = mdr
 
+    
+    def handle_ldi(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        
+        self.register[operand_a] = operand_b
+
+    def handle_prn(self):
+        operand_a = self.ram_read(self.pc + 1)
+        print(self.register[operand_a])
+
+    def handle_hlt(self):
+        self.halted = True
+
+    def handle_mul(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("MUL", operand_a, operand_b)
+
+    def run(self):
+        while self.halted != True:
+            IR = self.ram[self.pc]
+            val = IR
+            op_count = val >> 6
+            IR_length = op_count + 1
+            self.branchtable[IR]()
+
+            if IR == 0 or None:
+                print(f"Unknown instructions and index {self.pc}")
+                sys.exit(1)
+            self.pc += IR_length
+
+
+
+        #####SAVED FOR POSTERITY#######
+
+        #while cpu is running
+        # while running:
+        #     #  set instruction register per step 3
+        #     IR = self.ram[self.pc]
+
+        #     # set operand_a to pc+1 per step 3
+        #     operand_a = self.ram_read(self.pc + 1)
+        #     # set operand_b to pc+2 per step 3
+        #     operand_b = self.ram_read(self.pc + 2)
+
+        #     # if the instruction register is LDI
+        #     if IR == LDI:
+        #         #set register of operand_a to operand_b, jump 3 in PC (to PRN currently)
+        #         self.register[operand_a] = operand_b
+        #         self.pc +=3
+
+        #     # if the instruction register is PRN
+        #     elif IR == PRN:
+        #         #print the register of operand_a, jump 2 in PC
+        #         print(self.register[operand_a])
+        #         self.pc +=2
+
+        #     # if the instruction register is the halt command
+        #     elif IR == HLT:
+        #         #set running to false and exit
+        #         running = False
+        #         sys.exit(0)
+        #     # if anything else, invalid command and quit with failure code 1
+        #     else:
+        #         print(f"Invalid Command: {self.ram[self.pc]}")
+        #         sys.exit(1)
